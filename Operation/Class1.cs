a94d99ab30a292305f1160f031e6ffa4a5ff6157
LeeJeongGyun <jeonggyun0630@gmail.com>
2024-10-05 22:52:41 +0900

    초기 커밋.

---
+++ Operation/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Operation;
+
+public class AddClass
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public class SubClass
+{
+    public static int Sub(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+public class MultiplyClass
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+public class DivideClass
+{
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,51 @@
+namespace TestProject1;
+
+using Operation;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestAdd()
+    {
+        int num1 = 1;
+        int num2 = 2;
+
+        int retValue = AddClass.Add(num1, num2);
+
+        Assert.AreEqual(retValue, num1 + num2);
+    }
+
+    [TestMethod]
+    public void TestSub()
+    {
+        int num1 = 1;
+        int num2 = 2;
+
+        int retValue = SubClass.Sub(num1, num2);
+
+        Assert.AreEqual(retValue, num1 - num2);
+    }
+
+    [TestMethod]
+    public void TestMultiply()
+    {
+        int num1 = 1;
+        int num2 = 2;
+
+        int retValue = MultiplyClass.Multiply(num1, num2);
+
+        Assert.AreEqual(retValue, num1 * num2);
+    }
+
+    [TestMethod]
+    public void TestDivide()
+    {
+        int num1 = 1;
+        int num2 = 2;
+
+        int retValue = DivideClass.Divide(num1, num2);
+
+        Assert.AreEqual(retValue, num1 / num2);
+    }
+}
